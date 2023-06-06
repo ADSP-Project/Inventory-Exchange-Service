926fff6974b9b54681666608d8d9cfe85ec38dde
freefreeP <66095628+freefreeP@users.noreply.github.com>
2023-06-07 00:42:38 +0200

    almost works

--- socks-shop/federationservice/src/pages/api/hello.ts
+++ socks-shop/federationservice/src/pages/api/hello.ts
@@ -6,12 +6,22 @@ type Data = {
   name: string
 }
 
+// table: sock
+// | sock_id     | varchar(40)  | NO   | PRI | NULL    |       |
+// | name        | varchar(20)  | YES  |     | NULL    |       |
+// | description | varchar(200) | YES  |     | NULL    |       |
+// | price       | float        | YES  |     | NULL    |       |
+// | count       | int(11)      | YES  |     | NULL    |       |
+// | image_url_1 | varchar(40)  | YES  |     | NULL    |       |
+// | image_url_2 | varchar(40)  | YES  |     | NULL    |       |
+
+
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
   if (req.method === 'GET') {
-    connection.query('SELECT * FROM socks', (err, rows) => {
+    connection.query('SELECT * FROM sock', (err, rows) => {
       if (err) {
         console.log('Error connecting to DB: ', err);
         return;
