7240ea26af980e0c7bcc1cc2cbe8c90c9213f180
Filippa Leuckfeld <filippaleuckfeld@Filippas-MacBook-Air.local>
2023-07-08 11:52:35 +0200

    fix: Prometheus

--- socks-shop/federationservice/src/pages/api/prom.ts
+++ socks-shop/federationservice/src/pages/api/prom.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 export const foovar =  function(req: NextApiRequest, status:number) {
     console.log("foovar");
-    console.log(req);
+    //console.log(req);
     const Prometheus = require('prom-client');
     Prometheus.register.getSingleMetric('http_request_total').labels(req.method, req.url, status).inc();
 }
