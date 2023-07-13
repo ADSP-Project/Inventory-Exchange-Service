260630318c2c64b0285132163d15002eabc5fb8c
Filippa Leuckfeld <filippaleuckfeld@Filippas-Air.fritz.box>
2023-07-14 01:22:17 +0200

    fix: deployment

--- onlineboutique/src/federationservice/ui/dashboard/src/App.jsx
+++ onlineboutique/src/federationservice/ui/dashboard/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
   
   const [shops, setShops] = useState([]);
   console.log("HEJ!")
+  console.log("FEDERATIONSERVICE_BE_SERVICE_HOST")
   console.log(import.meta.env.VITE_FEDERATION_SERVICE)
 
   const MY_WEBHOOK_URL = `${import.meta.env.VITE_FEDERATION_SERVICE}/webhook`;
